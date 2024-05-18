a3dd102da45dee851483a4e1e340cec33d504296
AnisDaly <150529924+AnisDaly@users.noreply.github.com>
2024-05-18 15:46:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,15 @@
-#include <stdio.h>
-#include "column.h"
 #include "cdataframe.h"
 
 int main() {
-    CDataframe *df = create_cdataframe(3);
+    CDataframe cdf;
+    initialiserCDataframe(&cdf, 5); // Initialiser un CDataframe avec une capacité de 5 colonnes
 
+    Column *nouvelleColonne = creerColonne("Colonne d'exemple", 10);
+    insererDonnee(nouvelleColonne, (void*)0x123456); // Donnée exemple (adresse)
+    ajouterColonne(&cdf, nouvelleColonne);
 
-    Column *col1 = create_column("Age");
-    Column *col2 = create_column("Score");
-
-
-    insert_value(col1, 25);
-    insert_value(col1, 30);
-    insert_value(col1, 22);
-
-    insert_value(col2, 88);
-    insert_value(col2, 92);
-    insert_value(col2, 75);
-
-
-    add_column(df, col1);
-    add_column(df, col2);
-
-
-    print_cdataframe(df);
-
-
-    delete_cdataframe(&df);
+    afficherCDataframe(&cdf);
+    libererCDataframe(&cdf);
 
     return 0;
 }
